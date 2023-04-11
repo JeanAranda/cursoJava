7220c9c9169ed726b016d5462d4d2f74ea049cc8
JeanAranda <jarandar2014@gmail.com>
2023-04-11 02:11:13 -0500

    Impresion de objetos

--- OOP/Persona.java
+++ OOP/Persona.java
@@ -10,4 +10,18 @@ class Persona {
 		System.out.println("Edad: "+edad);
 		System.out.println("Genero: "+genero);
 	}
+
+	public static void main(String[] args) {
+		Persona p = new Persona();
+		p.nombre="Jean";
+		p.edad=31;
+		p.genero='M';
+		p.imprimirInformacion();
+
+		Persona p1=new Persona();
+		p1.nombre="Juan";
+		p1.edad=61;
+		p1.genero='F';
+		p1.imprimirInformacion();
+	}
 }
