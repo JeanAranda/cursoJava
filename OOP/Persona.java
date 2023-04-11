a4f68403645e925861f4671d621faac1ffd36769
JeanAranda <jarandar2014@gmail.com>
2023-04-11 01:57:33 -0500

    Creacion de nueva carpeta para POO

---
+++ OOP/Persona.java
@@ -0,0 +1,13 @@
+class Persona {
+	//Atributos
+	String nombre;
+	int edad;
+	char genero;
+
+	//Metodos
+	void imprimirInformacion(){
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Edad: "+edad);
+		System.out.println("Genero: "+genero);
+	}
+}
